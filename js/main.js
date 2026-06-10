0229008b175afac5cf71884717f4098b969a0419
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2026-06-10 11:47:42 -0300

    feat: adiciona carrossel dos 12 dourados

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-const navbar = document.querySelector(".site-header__nav");
-
-function toggleNavbarLinksOnScroll() {
-  if (!navbar) {
-    return;
-  }
-
-  navbar.classList.toggle("is-scrolled", window.scrollY > 20);
-}
-
-toggleNavbarLinksOnScroll();
-window.addEventListener("scroll", toggleNavbarLinksOnScroll);
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 6,
+    loop: true,
+    margin: 24,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 16,
+        stagePadding: 48,
+      },
+      600: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+      1200: {
+        items: 6,
+      },
+    },
+  });
+});
